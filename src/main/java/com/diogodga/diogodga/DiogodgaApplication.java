16e407fb3c945646b17fb4de4782ebffe9126ec4
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2021-12-26 00:27:59 -0300

    Projeto criado

---
+++ src/main/java/com/diogodga/diogodga/DiogodgaApplication.java
@@ -0,0 +1,13 @@
+package com.diogodga.diogodga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiogodgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiogodgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diogodga/diogodga/DiogodgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogodga.diogodga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiogodgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
