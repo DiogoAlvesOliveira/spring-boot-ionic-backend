ea87e037bce0759fb1d4e8c00a636dcca662eccc
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2021-12-26 22:09:16 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/diogodga/diogodga/com/resources/CategoriaResource.java
+++ src/main/java/com/diogodga/diogodga/com/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.diogodga.diogodga.com.resources;
 
+import com.diogodga.diogodga.domain.Categoria;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando";
+    @GetMapping
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(1, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
