689983cc93841fd2be20445b469a640c656d623b
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2021-12-27 22:24:31 -0300

    Criando operacao de instanciacao

--- src/main/java/com/diogodga/diogodga/DiogodgaApplication.java
+++ src/main/java/com/diogodga/diogodga/DiogodgaApplication.java
@@ -1,13 +1,31 @@
 package com.diogodga.diogodga;
 
+import com.diogodga.diogodga.domain.Categoria;
+import com.diogodga.diogodga.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class DiogodgaApplication {
+public class DiogodgaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiogodgaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
