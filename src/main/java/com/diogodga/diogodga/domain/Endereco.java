aa19b7cdf2018ff697d57ce605878f4f9161fbac
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2022-01-05 22:37:43 -0300

    utilizando JsonIgnore

--- src/main/java/com/diogodga/diogodga/domain/Endereco.java
+++ src/main/java/com/diogodga/diogodga/domain/Endereco.java
@@ -1,6 +1,5 @@
 package com.diogodga.diogodga.domain;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -20,7 +19,7 @@ public class Endereco implements Serializable {
     private String bairro;
     private String cep;
 
-    @JsonBackReference
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
