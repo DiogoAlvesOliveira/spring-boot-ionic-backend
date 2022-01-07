6587d495234128ff2d0b74e5fef428807d8a5a25
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2022-01-06 23:31:53 -0300

    Inserindo nova Categoria com post

--- src/main/java/com/diogodga/diogodga/resources/CategoriaResource.java
+++ src/main/java/com/diogodga/diogodga/resources/CategoriaResource.java
@@ -4,10 +4,10 @@ import com.diogodga.diogodga.domain.Categoria;
 import com.diogodga.diogodga.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -19,5 +19,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> find(@PathVariable Integer id){
         Categoria obj = categoriaService.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = categoriaService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
