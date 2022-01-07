43f622992b8436adb1b5ccb7b477e9066860b0c4
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2022-01-06 23:05:14 -0300

    Testando novo repositório

--- src/main/java/com/diogodga/diogodga/domain/Cliente.java
+++ src/main/java/com/diogodga/diogodga/domain/Cliente.java
@@ -31,6 +31,7 @@ public class Cliente implements Serializable {
     private List<Pedido> pedidos = new ArrayList<>();
 
     public Cliente() {
+
     }
 
     public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
