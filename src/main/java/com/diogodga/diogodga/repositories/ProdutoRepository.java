a3d1595d26c41e040c80d75ecc0abca8515c6654
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2022-02-22 12:46:01 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/diogodga/diogodga/repositories/ProdutoRepository.java
+++ src/main/java/com/diogodga/diogodga/repositories/ProdutoRepository.java
@@ -1,9 +1,22 @@
 package com.diogodga.diogodga.repositories;
 
+import com.diogodga.diogodga.domain.Categoria;
 import com.diogodga.diogodga.domain.Produto;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+    @Transactional(readOnly=true)
+    @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE UPPER(obj.nome) LIKE UPPER(concat('%',:nome,'%')) AND cat IN :categorias")
+    Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
